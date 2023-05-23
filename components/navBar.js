2b8d099ab5966227eafb9abc0590bfb30537fc1d
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-23 11:42:53 +0530

    14/206 navbar routing done Home,Mylist

--- components/navBar.js
+++ components/navBar.js
@@ -1,33 +1,46 @@
 import styles from './navBar.module.css'
-const NavBar=(props)=>{
-    const {username}=props;
+import { useRouter } from "next/router";
+import Link from "next/link";
+const NavBar = (props) => {
+    const { username } = props;
+    const router = useRouter();
+    const handleOnClickHome = (e) => {
+        e.preventDefault();
+        router.push("/");
+    };
+    const handleOnClickMyList = (e) => {
+        e.preventDefault();
+        router.push("/browse/my-list");
+    };
     return (
         <div className={styles.container}>
-             <div className={styles.wrapper}>
-                <a className={styles.logoLink} href="/"></a>
+            <div className={styles.wrapper}>
+                <a className={styles.logoLink}></a>
                 <div className={styles.logoWrapper}>
                     Netflix
                 </div>
-             
-             <ul className={styles.navItems}>
-                <li className={styles.navItem}>Home</li>
-                <li className={styles.navItem2}>My List</li>
-            </ul>
-            <nav className={styles.navContainer}>
-                <div>
-                    <button className={styles.usernameBtn}>
-                        <p className={styles.username}>{username}</p>
-                        {/* Expand more icons */}
-                    </button>
-                    <div className={styles.navDropdown}>
-                        <div>
-                        <a className={styles.linkName}>Sign Out</a>
-                        <div className={styles.lineWrapper}></div>
+
+                <ul className={styles.navItems}>
+                    <li className={styles.navItem} onClick={handleOnClickHome}>Home</li>
+                    <li className={styles.navItem2} onClick={handleOnClickMyList}>My List</li>
+                </ul>
+                <nav className={styles.navContainer}>
+                    <div>
+                        <button className={styles.usernameBtn}>
+                            <p className={styles.username}>{username}</p>
+                            {/* Expand more icons */}
+                        </button>
+                        <div className={styles.navDropdown}>
+                            <div>
+                                <Link href="/login" className={styles.linkName}>
+                                    Sign out
+                                </Link>
+                                <div className={styles.lineWrapper}></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </nav>
-</div>
+                </nav>
+            </div>
         </div>
     );
 }
