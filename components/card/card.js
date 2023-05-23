85732ca43392fd60562f063139adb11e31c23387
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-23 16:09:53 +0530

    14/113 card component image error

--- components/card/card.js
+++ components/card/card.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from './card.module.css'
 import { useState } from "react"; 
 const Card = (props) => {
-    const { imgUrl='/static/animal.webp', size='medium' } = props;
+    const { imgUrl='https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80', size='medium' } = props;
     const [imgSrc,setImgSrc]=useState(imgUrl);
     const classMap = {
         large: styles.lgItem,
@@ -11,7 +11,7 @@ const Card = (props) => {
     };
     const handleOnError = () => {
         console.log("hii error");
-        setImgSrc("/static/clifford.webp");
+        setImgSrc("https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80");
       };
     return <div className={styles.container}>card here
         <div className={classMap[size]}>
