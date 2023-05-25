01954a9c7e26528998c6d196dbcb24b884a4d6a8
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-25 15:40:59 +0530

    15/242 sign In page skeleton

--- pages/login.js
+++ pages/login.js
@@ -3,6 +3,10 @@ import Image from 'next/image';
 import Head from "next/head";
 import Link from 'next/link';
 const Login = () => {
+    const handleLoginWithEmail=(e)=>{
+        console.log("Say hii! button");
+        e.preventDefault();
+    }
     return <div>
         <Head>
             <title>Netflix SignIn</title>
@@ -20,6 +24,20 @@ const Login = () => {
                     </div>
                 </Link>
             </div>
+
+
+        <main className={styles.main}>
+            <div className={styles.mainWrapper}>
+             <h1 className={styles.signinHeader}>Sign In </h1>
+             <input type="text" placeholder='Email Address' className={styles.emailInput}/>
+             <p className={styles.userMsg}></p>
+             <button onClick={handleLoginWithEmail} className={styles.loginBtn}>Sign In</button>
+             </div>
+        </main>
+
+
+
+
         </header>
     </div>
 }
