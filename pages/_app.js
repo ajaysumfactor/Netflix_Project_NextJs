d5f6ae1830dbfa6a4c89f31e1a9001d658957f8e
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-26 14:46:02 +0530

    15/259 App route once login if user is not logged in render to login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { magic } from "../lib/magic-client";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+         router.push("/");
+      } else {
+         router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
   return <Component {...pageProps} />
 }
