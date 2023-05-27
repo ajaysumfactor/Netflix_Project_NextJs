80c402742be34c39c6c9759d3f0d0f5a4670906d
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-27 18:16:04 +0530

    16/271 install react Modal component

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,9 +1,23 @@
 import {useRouter} from 'next/router';
+import Modal from 'react-modal';
+import styles from '../../styles/Video.module.css'
+Modal.setAppElement('#__next');
+
  
 const Video = () =>{
     const router=useRouter();
     console.log({router});
-    return <div>video page {router.query.videoId}</div>
+    return(
+    <div>video page {router.query.videoId}
+    <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        overlayClassName={styles.overlay}  >
+        <div>must visible</div>
+          </Modal>
+    </div>
+    );
 };
 
 export default Video;
