07f11556d7009445603549fad334e64ba6c09d46
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-28 11:10:52 +0530

    16/274 implement youtube Api player

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -15,6 +15,16 @@ const Video = () =>{
         onRequestClose={() => router.back()}
         overlayClassName={styles.overlay}
         className={styles.modal} >
+
+
+<iframe
+          id="ytplayer"
+          type="text/html"
+          width="100%"
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=1&rel=1`}
+        //   frameBorder="0"
+        ></iframe>
         <div>must visible</div>
           </Modal>
     </div>
@@ -22,3 +32,5 @@ const Video = () =>{
 };
 
 export default Video;
+
+// https://developers.google.com/youtube/iframe_api_reference
