3055bbcd3fca497a3be3f4205fa691664ac3a96d
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-28 11:20:25 +0530

    16/275 youtube player styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -19,13 +19,14 @@ const Video = () =>{
 
 <iframe
           id="ytplayer"
+          className={styles.videoPlayer}
           type="text/html"
           width="100%"
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=1&rel=1`}
-        //   frameBorder="0"
+          frameBorder="0"
         ></iframe>
-        <div>must visible</div>
+        {/* <div>must visible</div> */}
           </Modal>
     </div>
     );
