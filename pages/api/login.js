1e2ec6417de98996e29a2c17d6c382bf2e128506
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-30 17:05:56 +0530

    18/316 Login API Create Auth headers

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,9 @@
 export default async function login(req, res) {
     if (req.method === "POST") {
         try {
-
+            const auth = req.headers.authorization;
+            const token = auth ? auth.substr(7): '';
+            console.log({token});
             res.send({ done: true });
         }
         catch (error) {
