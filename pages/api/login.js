b082cbb6f85411e0443038d639f6656d4555b144
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-30 12:55:40 +0530

    17/314 set up skeleton login API test in postman

---
+++ pages/api/login.js
@@ -0,0 +1,16 @@
+export default async function login(req, res) {
+    if (req.method === "POST") {
+        try {
+
+            res.send({ done: true });
+        }
+        catch (error) {
+            console.error("something went wrong logging in", error);
+            res.status(500).send({ done: false });
+
+        }
+    }
+    else {
+        res.send({ done: false });
+    }
+}
