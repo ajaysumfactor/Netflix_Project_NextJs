13ded1497d2b114dfaa0e71e7893192120709708
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-31 11:13:18 +0530

    18/318 get metadata__issuer,email,pubAddress.. from didToken and tested in console by passing didToken as a Authorization in postman

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,13 @@
+import { mAdmin } from "@/lib/magic";
 export default async function login(req, res) {
     if (req.method === "POST") {
         try {
-            const auth = req.headers.authorization;
-            const token = auth ? auth.substr(7): '';
-            console.log({token});
+            const auth = req.headers.authorization; //Barrer Token
+            const didToken = auth ? auth.substr(7): ''; //Extract Token here in this it is didToken
+            console.log({didToken});
             //invoke magic here
+            const metadata = await mAdmin.users.getMetadataByToken(didToken);
+            console.log({metadata});//get all the metadata(issuer,publicAddress,email);
             res.send({ done: true });
         }
         catch (error) {
