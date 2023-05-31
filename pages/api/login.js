2fa746ed901f915e5e1c636252a990d96286c621
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-31 15:07:52 +0530

    18/321 create JWT

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { mAdmin } from "@/lib/magic";
 export default async function login(req, res) {
     if (req.method === "POST") {
@@ -8,6 +9,24 @@ export default async function login(req, res) {
             //invoke magic here
             const metadata = await mAdmin.users.getMetadataByToken(didToken);
             console.log({metadata});//get all the metadata(issuer,publicAddress,email);
+
+
+
+            //create Jwt
+
+            const token = jwt.sign(
+                {
+                     ...metadata,
+                    iat: Math.floor(Date.now() / 1000),
+                    exp: Math.floor(Date.now()/1000 + 7*24*60*60),
+                    "https://hasura.io/jwt/claims": {
+                      "x-hasura-default-role": "user",
+                      "x-hasura-allowed-roles": ["user", "admin"],
+                      "x-hasura-user-id": `${metadata.issuer}`,
+                      },
+                  },'abcdefghabcdefghabcdefgh12345678'
+            );
+            console.log({token});
             res.send({ done: true });
         }
         catch (error) {
