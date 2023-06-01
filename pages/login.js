555875a23a27c6a430176a0dff8a009581606871
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-01 15:59:38 +0530

    18/331 invoked login API on login

--- pages/login.js
+++ pages/login.js
@@ -2,9 +2,9 @@ import styles from '../styles/Login.module.css'
 import Image from 'next/image';
 import Head from "next/head";
 import Link from 'next/link';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import {magic} from "../lib/magic-client.js";
+import { magic } from "../lib/magic-client.js";
 
 
 
@@ -13,22 +13,22 @@ const Login = () => {
 
     const [email, setEmail] = useState("");
     const [userMsg, setUserMsg] = useState("");
-    const [isLoading,setIsLoading]=useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
-        async function loginDelay(){
-        const handleComplete = () => {
-          setIsLoading(false);
-        };
-        router.events.on("routeChangeComplete", handleComplete);
-        router.events.on("routeChangeError", handleComplete);
-    
-        return () => {
-          router.events.off("routeChangeComplete", handleComplete);
-          router.events.off("routeChangeError", handleComplete);
-        };
-    }
-    loginDelay();
-      }, [router]);
+        async function loginDelay() {
+            const handleComplete = () => {
+                setIsLoading(false);
+            };
+            router.events.on("routeChangeComplete", handleComplete);
+            router.events.on("routeChangeError", handleComplete);
+
+            return () => {
+                router.events.off("routeChangeComplete", handleComplete);
+                router.events.off("routeChangeError", handleComplete);
+            };
+        }
+        loginDelay();
+    }, [router]);
 
     const handleOnChangeEmail = (e) => {
         //    console.log("event",e);
@@ -36,34 +36,47 @@ const Login = () => {
         const email = e.target.value;
         setEmail(email);
     }
-    const handleLoginWithEmail = async(e) => {
+    const handleLoginWithEmail = async (e) => {
         console.log("Say hii! button");
         e.preventDefault();
         setIsLoading(true);
         if (email) {
-            if(email==='tigersis1995@gmail.com'){
+
             //    console.log("user logged in ")
             //    router.push("/");
             try {
                 const DIDToken = await magic.auth.loginWithMagicLink({
                     email,
-                 });
-                 console.log(DIDToken);
-                 if(DIDToken){//render to home page if DIDToken is successfully generated 
+                });
+                console.log(DIDToken);
+                if (DIDToken) {//render to home page if DIDToken is successfully generated 
                     // setIsLoading(false);
-                    router.push("/");
-                 }
-              } catch(error) {
-                setIsLoading(false);
-                console.log("DIDTOKEN Error",error);
-               }
+                    const response = await fetch("/api/login", {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': `Bearer ${DIDToken}`,
+                            'Content-Type': 'application/json',
+                        },
+                    });
+                    const loggedInResponse = await response.json();
+                    if (loggedInResponse.done) {
+                        console.log({ loggedInResponse });
+                        router.push("/");
 
-            }
-            else{
+                    }
+                    else {
+                        setIsLoading(false);
+                        setUserMsg("something went wrong logging in ")
+                    }
+                    // router.push("/");
+                }
+            } catch (error) {
                 setIsLoading(false);
-
-                setUserMsg("something went wrong!");
+                console.log("DIDTOKEN Error", error);
             }
+
+
+
 
         }
         else {
