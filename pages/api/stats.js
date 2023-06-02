e397e263dfc0a08fdab4c0102c88741021794cd6
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-02 12:48:54 +0530

    19/339 build stats for request cookies get cookies from the browser

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,18 @@
 export default async function stats(req,res){
     if(req.method==='POST'){
+        console.log({cookies: req.cookies});
+        try{
+        if(!req.cookies.token){
+            res.status(403).send({});
+        }
+        else{
         res.send({msg:"it's working"});
+        }
+    }
+    catch(error)
+    {
+        console.error("Error occured /stats",error);
+        res.status(500).send({done: false,error:error?.message});
+    }
     }
 }
