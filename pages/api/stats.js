02328aa66162579135ce4967f5d8da971d1baedd
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-02 13:22:38 +0530

    19/341 stats API JWT Verification and verify in postman

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,18 +1,21 @@
-export default async function stats(req,res){
-    if(req.method==='POST'){
-        console.log({cookies: req.cookies});
-        try{
-        if(!req.cookies.token){
-            res.status(403).send({});
+import jwt from 'jsonwebtoken';
+export default async function stats(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const token = req.cookies.token;
+
+            if (!token) {
+                res.status(403).send({});
+            }
+            else {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                console.log({ decoded });
+                res.send({ msg: "it's working", decoded });
+            }
         }
-        else{
-        res.send({msg:"it's working"});
+        catch (error) {
+            console.error("Error occured /stats", error);
+            res.status(500).send({ done: false, error: error?.message });
         }
-    }
-    catch(error)
-    {
-        console.error("Error occured /stats",error);
-        res.status(500).send({done: false,error:error?.message});
-    }
     }
 }
