899b0907490e9d4ee8807637f24fbe41a6af91dc
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-02 16:22:11 +0530

    19/344 find user by Id Graph Ql verified in postman

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { findVideoIdByUser } from '@/lib/db/hasura';
 export default async function stats(req, res) {
     if (req.method === 'POST') {
         try {
@@ -10,7 +11,10 @@ export default async function stats(req, res) {
             else {
                 const decoded = jwt.verify(token, process.env.JWT_SECRET);
                 console.log({ decoded });
-                res.send({ msg: "it's working", decoded });
+                const userId = "did:ethr:0xFF9D7617bED57730B9DbedE6fA4327E9eB5c6B50";
+                const videoId = "4zH5iYM4wJo";
+                const findVideoId = await findVideoIdByUser(token,userId,videoId);
+                res.send({ msg: "it's working", decoded,findVideoId });
             }
         }
         catch (error) {
