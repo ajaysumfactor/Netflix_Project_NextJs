7b7c7d43083a068c03cd3fcb6a75176e24b3db19
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-05 15:41:03 +0530

    19/361 watch it again call in server side rendering

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 import NavBar from '@/components/navBar'
 import Card from '@/components/card/card.js'
 import SectionCards from "../components/card/section-cards";
-import { getVideos ,getPopularVideos} from "../lib/videos";
+import {getWatchItAgainVideos, getVideos ,getPopularVideos} from "../lib/videos";
 import { startFetchMyQuery } from "../lib/db/hasura";
 
  
 export async function getServerSideProps() {
+  const userId="did:ethr:0xFF9D7617bED57730B9DbedE6fA4327E9eB5c6B50";
+  const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJkaWQ6ZXRocjoweEZGOUQ3NjE3YkVENTc3MzBCOURiZWRFNmZBNDMyN0U5ZUI1YzZCNTAiLCJwdWJsaWNBZGRyZXNzIjoiMHhGRjlENzYxN2JFRDU3NzMwQjlEYmVkRTZmQTQzMjdFOWVCNWM2QjUwIiwiZW1haWwiOiJ0aWdlcnNpczE5OTVAZ21haWwuY29tIiwib2F1dGhQcm92aWRlciI6bnVsbCwicGhvbmVOdW1iZXIiOm51bGwsIndhbGxldHMiOltdLCJpYXQiOjE2ODU3MTEyNTAsImV4cCI6MTY4NjMxNjA1MCwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJ1c2VyIiwiYWRtaW4iXSwieC1oYXN1cmEtdXNlci1pZCI6ImRpZDpldGhyOjB4RkY5RDc2MTdiRUQ1NzczMEI5RGJlZEU2ZkE0MzI3RTllQjVjNkI1MCJ9fQ.ldqjOVGxnxTePsaoj8IymaUsMSgpSrNQyq9du96_nic";
+  const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
+
+  console.log({ watchItAgainVideos });
   const disneyVideos = await getVideos("disney trailer");
   const travelVideos = await getVideos("travel trailer");
   const productivityVideos = await getVideos("productivity trailer");
@@ -19,13 +24,14 @@ export async function getServerSideProps() {
 
 
 
-  return { props: { disneyVideos, travelVideos, productivityVideos, popularVideos } };
+  return { props: { disneyVideos, travelVideos, productivityVideos, popularVideos,watchItAgainVideos } };
 }
 
-export default function Home({ disneyVideos, travelVideos, productivityVideos, popularVideos }) {
+export default function Home({ disneyVideos, travelVideos, productivityVideos, popularVideos,watchItAgainVideos }) {
   // console.log(disneyVideos);
   // console.log({magic});
   // startFetchMyQuery();
+  console.log({watchItAgainVideos});
 
    return (
     <>
