f627faaf097fbc7bdcb80925d30b2530cb3d2aba
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-06 13:53:21 +0530

    19/373 wrap card in mylist page

--- components/card/card.js
+++ components/card/card.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import cls from "classnames";
 const Card = (props) => {
-    const { imgUrl = 'https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80', size = 'medium',id } = props;
+    const { imgUrl = 'https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80', size = 'medium',id , shouldScale=true, } = props;
     const [imgSrc, setImgSrc] = useState(imgUrl);
     const classMap = {
         large: styles.lgItem,
@@ -16,9 +16,11 @@ const Card = (props) => {
         setImgSrc("https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80");
     };
     const scale=id===0? { scaleY: 1.1} : { scale: 1.1}
+
+    const shouldHover = shouldScale && {whileHover: {...scale},};
     return <div className={styles.container}>
         <motion.div
-            className={cls(styles.imgMotionWrapper, classMap[size])} whileHover={...scale}>
+            className={cls(styles.imgMotionWrapper, classMap[size])} {...shouldHover}>
 
             <Image src={imgSrc}
                 alt="image"
--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,20 +1,21 @@
 import Card from './card'
 import styles from './section-cards.module.css'
 import Link from 'next/link';
+ import clsx from "classnames";
 
 
 const SectionCards = (props) => {
-    const { title,videos=[],size } = props; //error handle if videos does not have any content 
+    const { title,videos=[],size,shouldWrap=false,shouldScale } = props; //error handle if videos does not have any content 
     // console.log(videos);
     return (
         <section className={styles.container}>
             <h2 className={styles.title}>{title}</h2>
-            <div className={styles.cardWrapper}>
+            <div className={clsx(styles.cardWrapper,shouldWrap && styles.wrap)}>
 
             {videos.map((video,idx)=>{
                 // console.log({video});
                 return <Link key={idx} href={`/video/${video.id}`}>
-                <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
+                <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} shouldScale={shouldScale} />
                 </Link>
 
             })}
