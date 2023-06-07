535b06bb39de1d60daf9ac021a5a2aae470bae9c
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-07 13:03:57 +0530

    20/381 try out middle ware in project

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req,ev){
+    console.log({req,ev});
+    //check the token
+    //if token is valid
+    //|| if page is /login
+    // return NextResponse.next();
+    return new Response("hello world");
+
+    //if no token
+    //redirect to login
+}
