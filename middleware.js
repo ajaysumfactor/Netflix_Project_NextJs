08ffee67009c9e817aa874785275cbb92d1ca378
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-07 15:21:36 +0530

    20/382 middleware implemented

--- middleware.js
+++ middleware.js
@@ -1,13 +1,24 @@
 import { NextResponse } from "next/server";
+import { verifyToken } from "./lib/utils";
 
-export function middleware(req,ev){
-    console.log({req,ev});
+export async function middleware(req,ev){
+    const token = req ? req.cookies.token : null;
+      const userId= await verifyToken(token);
+      const {pathname} = req.nextUrl;
+      if(token && userId || pathname.includes("/api/login") ){
+        console.log("here");
+
+        return NextResponse.next();
+
+      }
     //check the token
     //if token is valid
     //|| if page is /login
-    return NextResponse.next();
     // return new Response("hello world");
 
+    if(!token && pathname !== pathname){
+    return NextResponse.redirect("/login");
+    }
     //if no token
     //redirect to login
 }
--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -8,15 +8,7 @@ import { redirectUser } from "@/utils/redirectUser";
 
 export async function getServerSideProps(context){
     const {userId,token} = await redirectUser(context);
-    if (!userId) {
-        return {
-          props: {},
-          redirect: {
-            destination: "/login",
-            permanent: false,
-          },
-        };
-      }
+   
     const videos = await getMyList(userId,token);
     return {
       props: {
