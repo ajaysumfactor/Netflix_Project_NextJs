aacbd065bc95858b7a540e5d425ec014a9b66d9a
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-06-08 10:30:32 +0530

    issue commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+ import styles from '@/styles/Home.module.css'
 import Banner from '@/components/banner'
-const inter = Inter({ subsets: ['latin'] })
 import NavBar from '@/components/navBar'
-import Card from '@/components/card/card.js'
-import SectionCards from "../components/card/section-cards";
+ import SectionCards from "../components/card/section-cards";
 import {getWatchItAgainVideos, getVideos ,getPopularVideos} from "../lib/videos";
-import { startFetchMyQuery } from "../lib/db/hasura";
-import { verifyToken } from '@/lib/utils'
+ 
 import { redirectUser } from '@/utils/redirectUser'
 
  
